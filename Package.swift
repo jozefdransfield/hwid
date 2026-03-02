7706dec9501367ef2ca0c84a3d4bc5c290b50e99
Jozef Dransfield <Jozef.Dransfield@me.com>
2026-03-02 15:15:51 +0000

    Tweeking package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,12 +8,16 @@ let package = Package(
         .macOS(.v10_15),
         .iOS(.v13)
     ],
+    products: [
+          .library(
+              name: "HWiD",
+              targets: ["HWiD"]
+          ),
+      ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.6.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target (
             name: "HWiD",
             dependencies: [
